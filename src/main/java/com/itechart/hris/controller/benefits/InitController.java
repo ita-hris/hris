ce51a90c839b20d1ca6e9879d196acbede9b161a
Ihar Yudziankou <Ihar.Yudziankou@itechart-group.com>
2020-01-23 15:40:35 +0300

    rename packages using project name

--- src/main/java/com/itechart/hris/controller/benefits/InitController.java
+++ src/main/java/com/itechart/hris/controller/benefits/InitController.java
@@ -1,4 +1,4 @@
-package com.itechart.benefits.controller;
+package com.itechart.hris.controller.benefits;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
