fa9e3863b78ffaacba4fcc8137a8ef9385587fd6
artsiom.vinahradau <artsiom.vinahradau@external.wirecard.com>
2020-01-27 12:44:48 +0300

    Added simple swagger example - Rest documentation in init controller - Added auto-test possibility

--- src/main/java/com/itechart/hris/controller/benefits/InitController.java
+++ src/main/java/com/itechart/hris/controller/benefits/InitController.java
@@ -1,13 +1,18 @@
 package com.itechart.hris.controller.benefits;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/benefits")
+@Api(value = "/benefits")
 public class InitController {
 
-    @RequestMapping("/init")
+    @ApiOperation(value = "Get API greeting")
+    @GetMapping(value = "/init")
     public String init() {
         return "Hello World";
     }
