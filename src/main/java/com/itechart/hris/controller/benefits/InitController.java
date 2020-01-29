09091524691dea83dfb96614d6b5d084da5c413b
artsiom.vinahradau <artsiom.vinahradau@external.wirecard.com>
2020-01-29 17:29:23 +0300

    Small fix

--- src/main/java/com/itechart/hris/controller/benefits/InitController.java
+++ src/main/java/com/itechart/hris/controller/benefits/InitController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/benefits")
-@Api(value = "/benefits")
+@Api
 public class InitController {
 
     @ApiOperation(value = "Get API greeting")
