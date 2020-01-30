cc15768c261e9ccf8efe7c82be0d636ac245475a
Artsiom Vinahradau <44962632+avinahradau@users.noreply.github.com>
2020-01-30 11:42:02 +0300

    Added simple swagger example (#4)
    * Added simple swagger example
    - Rest documentation in init controller
    - Added auto-test possibility
    
    * about controller
    
    * About controller documentation
    
    * Small fix
    
    Co-authored-by: IharYudziankou <Ihar.Yudziankou@gmail.com>

--- src/main/java/com/itechart/hris/controller/AboutController.java
+++ src/main/java/com/itechart/hris/controller/AboutController.java
@@ -1,11 +1,14 @@
 package com.itechart.hris.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api
 public class AboutController {
 
   private final BuildProperties buildProperties;
@@ -14,7 +17,7 @@ public class AboutController {
   public AboutController(final BuildProperties buildProperties) {
     this.buildProperties = buildProperties;
   }
-
+  @ApiOperation(value = "Get build properties")
   @GetMapping("/about")
   public BuildProperties about() {
     return buildProperties;
